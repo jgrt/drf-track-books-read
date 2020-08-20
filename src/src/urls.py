e7e28a15e6de1b03c3428eb3d6530278603f2066
jgrt <asati.jagrati93@gmail.com>
2020-08-20 05:39:49 +0530

    OpenAPI 3(Swagger) Setup. Dynamic API document generation.

--- src/src/urls.py
+++ src/src/urls.py
@@ -15,7 +15,21 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework.schemas import get_schema_view
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('openapi-schema', get_schema_view(
+        title="Tutorial app",  # Title of your app
+        description="tutorial app",  # Description of your app
+        version="1.0.0",
+        public=True,
+    ), name='openapi-schema'),
+    path('docs/', TemplateView.as_view(
+        template_name='swagger-ui.html',
+        extra_context={'schema_url': 'openapi-schema'}
+    ), name='swagger-ui'),
 ]
