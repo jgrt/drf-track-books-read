eacfa107fcd0834d4ac517974e6a043049e3ce66
jgrt <asati.jagrati93@gmail.com>
2020-08-20 07:19:06 +0530

    Created Book Schema

--- src/books/models.py
+++ src/books/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Book(models.Model):
+    name = models.CharField(max_length=256)
+    author = models.CharField(max_length=256)
+    is_read = models.BooleanField(default=False)
+    created = models.DateTimeField(default=timezone.now)
+    updated = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return "{} By {}".format(self.name, self.author)
+
+    class Meta:
+        ordering = ['created']
